98bfbe066e4f2ebd8f39a967241cdbb97704e6c6
naman <namangupta21200@gmail.com>
2024-04-20 17:52:24 +0530

    changes

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from 'express';
 import router from './routes/routes.js';
 import cors from 'cors';
 import DbConnection from './database/db.js';
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use('/',router);
